721366d204878476e342b0cc60927d1a10d1733f
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-04-20 17:26:59 -0500

    begin

---
+++ ingest.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import os
+
+subscribed = pd.read_csv( "downloads/subscribed.csv" )
+canceled = pd.read_csv( "downloads/canceled.csv" )
+first = pd.read_csv( "downloads/first.csv" )
