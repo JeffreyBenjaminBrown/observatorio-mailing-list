2949bd6274146e6146141c22b1e8d830d978bcf9
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-04-20 21:55:55 -0500

    limit to active subscriptions, write to disk

--- main.py
+++ main.py
@@ -1,30 +1,41 @@
 import pandas as pd
 import os
 
-# Download, merge
+# Download
 subscribed = pd.read_csv( "downloads/subscribed.csv" ) # collected online, ongoing
 canceled = pd.read_csv( "downloads/canceled.csv" ) # collected online, ongoing
 first_subscribed = pd.read_csv(
   "downloads/first.csv" # collected once, at the observatorio launch
   , usecols = [ 'Marca temporal', 'Nombres', 'Apellidos'
                 , 'Correo electrónico' ] )
+
+# Homogenize
 first_subscribed = first_subscribed.rename(
   columns={'Marca temporal':'Timestamp', 'Apellidos':'Primer Apellido'
            , 'Correo electrónico': 'Correo Electrónico'} )
-subscribed_small = subscribed # TODO: delete
 subscribed = subscribed.append( first_subscribed )
-del(first_subscribed)
 canceled = canceled.rename(
   columns={'Correo electrónico': 'Correo Electrónico'})
+del(first_subscribed)
 
-# Sort
-subscribed = subscribed.sort_values(by=['Correo Electrónico','Timestamp'])
-canceled   = canceled  .sort_values(by=['Correo Electrónico','Timestamp'])
-subscribed_small = subscribed_small.sort_values( # TODO: delete
-  by=['Correo Electrónico','Timestamp'] )
+# Unify
+subscribed['Cancellation']=False
+canceled['Cancellation']=True
+united = subscribed.append( canceled )
+del(subscribed,canceled)
 
-# Gruop
-subscribed = subscribed.groupby('Correo Electrónico').last()
-canceled   = canceled  .groupby('Correo Electrónico').last()
-subscribed_small = subscribed_small.groupby( # TODO: delete
-                                'Correo Electrónico').last()
+# Limit to active subscriptions
+united = united.sort_values(by=['Correo Electrónico','Timestamp'])
+united = united.groupby('Correo Electrónico').last()
+  # Find the last thing each email address did.
+united = united.reset_index()
+  # Gruoping turns the email address from a column to an index. Reverse that.
+united = united[ ~united['Cancellation'] ].drop(['Cancellation'],axis=1)
+  # If the last action from that address was to cancel, remove it.
+
+# Output
+if not os.path.exists('output'): os.makedirs('output')
+united.to_csv( 'output/active_subscriptions.csv' )
+addresses = united['Correo Electrónico'].tolist()
+with open("output/addresses.txt", "w") as text_file:
+    print(', '.join(addresses), file=text_file)
