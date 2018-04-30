c673e3a1c01cfd49b2298e3289b2cb42ea4d0e24
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-04-30 17:30:33 -0500

    modify the "python path"   so that code run by python3 from bash (in Docker)   behaves the same as code run from the python3 REPL (in Docker)

--- docker/go.sh
+++ docker/go.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+export PYTHONPATH=/mnt:/opt/conda/lib/python36.zip:/opt/conda/lib/python3.6:/opt/conda/lib/python3.6/lib-dynload:/opt/conda/lib/python3.6/site-packages
+  # why that: # https://stackoverflow.com/questions/30378105/python-modules-not-found-over-terminal-but-on-python-shell-linux
+
 cd /mnt/private/input && make downloads
-cd /mnt && python3 main.py
+cd /mnt && python3 src/main.py
